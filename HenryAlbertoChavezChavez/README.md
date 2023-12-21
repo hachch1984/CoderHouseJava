27a2d0fbc344d9d04984a9960dd1183156ef5dcd
Henry Alberto Chavez Chavez <hachch1984two@gmail.com>
2023-12-21 14:39:05 -0500

    Desafio: Manejo de tipo de datos float

--- HenryAlbertoChavezChavez/README.md
+++ HenryAlbertoChavezChavez/README.md
@@ -1,6 +1,6 @@
 ## Getting Started
 
-Welcome to the VS Code Java world. Here is a guideline to help you get started to write Java code in Visual Studio Code.
+Saludos, mi nombre es Henry y el proyecto a sido trabajado en Visual Studio Code
 
 ## Folder Structure
 
---
+++ HenryAlbertoChavezChavez/src/NumeroFlotanteHenryChavez.java
@@ -0,0 +1,12 @@
+public class NumeroFlotanteHenryChavez {
+    public static void main(String[] args) throws Exception {
+        float numberFloat = 3.14f;
+        int integerPart = (int) numberFloat;
+        float floatPart = numberFloat - integerPart;
+        
+        System.out.println("Integer part: " + integerPart);
+        System.out.println("Decimal part: " + floatPart);
+    }
+}
+
+
